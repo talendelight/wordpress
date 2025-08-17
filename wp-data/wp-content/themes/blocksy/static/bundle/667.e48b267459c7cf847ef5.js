64b52d7cc63e6ca1f344c6390322aa66f537db6a
code-sooraj <code.sooraj@gmail.com>
2025-08-18 00:13:06 +0300

    Added configurations for db data blob, wp config for dev, sample ftp config, default basic blocksy theme

---
+++ wp-data/wp-content/themes/blocksy/static/bundle/667.e48b267459c7cf847ef5.js
@@ -0,0 +1,1 @@
+"use strict";(self.blocksyJsonP=self.blocksyJsonP||[]).push([[667],{8667:function(n,o,s){s.r(o),s.d(o,{mount:function(){return f}});var t=s(2194),c=s.n(t);const f=function(){window._wapf&&window._wapf(c())}}}]);
